39a7e1c813cf4154155c5a354b499538e15e1873
Maximilien Roussel <ro.maximilien@gmail.com>
2022-07-18 18:15:27 +0200

    ex11

--- ex11.js
+++ ex11.js
@@ -1,12 +1,22 @@
 var prompt = require ('prompt-sync')();
 
 
-let age ; 
 
 const age2 = prompt("veuillir votre age   ");
+if (age2 < 3 ){
+console.log('vous êtes trop petit');
+} 
 
-if (age2 >=3 ) {
-console.log("vous êtes grand")    
-}else if 
-(age2>6  ) AND ( age2 < 9)
-    console.log("vous êtes poussin ")
+else if (age2 >=3 && age2<6) 
+console.log("vous êtes dans la categorie bebe");
+  else if (age2 >6 && age2<9)
+  console.log("vous êtes dans la categorie bebe");
+  else if (age2 >9 && age2<11)
+  console.log("vous êtes dans la categorie pupille");
+  else if (age2 >10 && age2<13)
+  console.log("vous êtes dans la categorie minime");
+ else if(age2 >13 && age2<17)
+ console.log("vous êtes cadet")
+ else if(age2 >17 )
+ console.log("vous êtes majeur")
+ 
