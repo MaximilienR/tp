d6393b1ea74f4be04eb2e1496f11077b196b26d7
Maximilien Roussel <ro.maximilien@gmail.com>
2022-07-18 17:30:23 +0200

    ex11

---
+++ ex11.js
@@ -0,0 +1,12 @@
+var prompt = require ('prompt-sync')();
+
+
+let age ; 
+
+const age2 = prompt("veuillir votre age   ");
+
+if (age2 >=3 ) {
+console.log("vous êtes grand")    
+}else if 
+(age2>6  ) AND ( age2 < 9)
+    console.log("vous êtes poussin ")
