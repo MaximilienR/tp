95dff6d93fc1334c87d28fa8fbafda05d5fc5ad9
Maximilien Roussel <ro.maximilien@gmail.com>
2022-07-18 17:17:03 +0200

    ex10

--- ex10.js
+++ ex10.js
@@ -6,8 +6,16 @@ var password ="azerty";
 const mail2 = prompt("veuillir votre mail  ");
 const password2 =prompt("veuillez saisir un mot de passe")
 
-if (mail != mail2 ) {
- console.log("mauvaise adresse")   
-}else{
-    console.log("bienvenu")
-}
+if (mail2 ===mail) {
+ console.log("welcom")  
+ if(password2===password){
+    console.log("mot de passe ok")
+ }else{
+    console.log("mail incoorecct")
+ } 
+
+}else if(
+    mail2!=mail){
+        console.log("mail incorrect")
+    }
+ 
