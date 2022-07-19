b007368f3cea235d80d6b112ce13433772e022cf
Maximilien Roussel <ro.maximilien@gmail.com>
2022-07-19 09:43:27 +0200

    ex23

---
+++ ex15.js
@@ -0,0 +1,31 @@
+var prompt = require ('prompt-sync')();
+//cat poids
+const poid = prompt("veuillir votre poids ");
+let poindKG =parseInt(poid);
+
+console.log("votre poid est de" + " " + poid + "kg");
+if(poindKG <43 ){
+    console.log("désolé votre taille n'est pas référencé")
+    die 
+}   
+
+
+//cat tail 
+
+const taille = prompt("veuillez saisir votre taille en cm") 
+let taillCM = parseInt(taille);
+
+console.log("votre taille est de" +" "+ taille +" " +"cm");
+
+if(taillCM <145 ){
+    console.log("désolé votre taille n'est pas référencé")
+    die 
+}   
+
+if(taillCM > 146  && taillCM <170 ){
+    console.log("vous faites une taille 1")
+}
+
+if(taillCM > 178    ){
+    console.log("vous faites une taille 1")
+}
---
+++ ex23.js
@@ -0,0 +1,8 @@
+var prompt = require ('prompt-sync')();
+const table = prompt("veuillir votre nombre  ");
+
+
+for (let index = 0; index <11; index++) {
+     console.log(table + "*"+index + "=" +index*table)
+     
+}
