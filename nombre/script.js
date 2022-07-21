72390fe0cf0c2a1baeb07f7c24c171aed00df140
Maximilien Roussel <ro.maximilien@gmail.com>
2022-07-21 14:58:41 +0200

    nombre mystère

--- nombre/script.js
+++ nombre/script.js
@@ -1,7 +1,38 @@
 alert("yoyuhou")
 
-function RandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
+/*var essais = 0;
+
+function incrementClick() {
+    updateDisplay(essais+=1);
+}*/
  
+var compteur=0;
+var solution ;
+var choixUser = document.getElementById("reponse").value;
+var nombre =function RandomInt(max) {
+    return Math.floor(Math.random() * 50);
+    console.log(max)
+
+   }
+  
+    function play() {
+        Number(choixUser)
+        if (choixUser<nombre) {
+            alert("choisisez un nombre plus grand");
+        } else if (choixUser>nombre){
+            alert("choissez un nombre plus petit");
+        } else if (choixUser == nombre){
+            alert("vous avez gagner");
+        }else if (choixUser==""){
+            alert("veuillez saisir un nombre");
+        }
+    }
+    //compteur
+    function test(){
+        Number
+        document.getElementById("chance").innerHTML += 1;}
+
+    //recharger la page 
+function reload(){
+window.location.reload()
+}
