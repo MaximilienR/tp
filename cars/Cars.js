ff34b0335a3bd902275b756bb9c54d430f0d6589
Maximilien Roussel <ro.maximilien@gmail.com>
2022-07-26 11:57:43 +0200

    exo cars finish

--- cars/Cars.js
+++ cars/Cars.js
@@ -1,25 +1,34 @@
 class Cars{ 
 
-    constructor(marque,model,vitesse){
+    constructor(marque,model,vitesse,compteur){
         this.marque=marque;
         this.model=model;
         this.vitesse=vitesse;
-         
+        this.compteur=0;
+          
     }     
+    /*
     get getVitesse(){
         return this.vitesse;
     }
     set setVitesse(vitesse){
         this.vitesse +=vitesse+10;
-    }
+    }*/
+    
+   
+    
+    
+    tourne(){
+        (this.vitesse -=5)
     }
 
-    /*accelerer(){
-        console.log(vitesse + 10)
-    }
-    //console.log(Cars)
+    
+ accelerer(){
+  (this.vitesse+=10)
+};
+}
 
-*/
+
 export default Cars
 
  
--- cars/model.js
+++ cars/model.js
@@ -1,6 +1,26 @@
 import Cars from "./Cars.js";
-
-let Voiture1 = new Cars("BMW", "Serie1",80,);
+ 
+ let Voiture1 = new Cars("BMW", "Serie1",80,);
 let Voiture2 = new Cars("Mercedes", " GLB",100,);
 
-console.log(Voiture1.marque);
+/*console.log('la voiture 1 roule a '+ Voiture1.vitesse );
+console.log('la voiture 2 roule a ' + Voiture2.vitesse)*/
+
+ 
+for (var nb = 0; nb < 4; nb++) {
+ Voiture1.accelerer();
+}
+
+for (let ac = 0; ac < 2; ac++) {
+    Voiture2.accelerer();
+    for (let tourne = 0; tourne <2; tourne++) {
+        Voiture2.tourne();        
+    }
+}
+console.log("la voiture 1 a accélérer 3 fois et obtient une vitesse de "+ Voiture1.vitesse + "km")
+console.log("la voiture 2 a accélérer 2 fois, tourné 2 fois  et obtient une vitesse de "+ Voiture2.vitesse + "km")
+
+
+ 
+
+ 
