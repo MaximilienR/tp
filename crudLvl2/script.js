c8173aee652f8363e2a036a4a12787d31eb277bb
Maximilien Roussel <ro.maximilien@gmail.com>
2022-07-27 16:33:15 +0200

    update ex2

---
+++ crudLvl2/script.js
@@ -0,0 +1,49 @@
+alert('foo')
+
+//modilisation
+class Article {
+
+    constructor(text) {
+        this.text = text;
+    }
+
+}
+
+var product = new Article();
+
+
+//afficher produit
+
+function creat() {
+     var art1= document.getElementById('prod').value;
+ document.getElementById("ecran").innerHTML += art1;
+
+};
+
+let AddArt = () => {
+    posts.innerHTML += `
+  <div>
+    <p>${ecran.text}</p>
+    <span class="options">
+       <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+    input.value = "";
+};
+
+let deletePost = (e) => {
+    e.parentElement.remove("ecran");
+
+};
+
+
+/* */
+let add = document.getElementById('test')
+let ecran =document.getElementById('ecran')
+
+add.addEventListener('click', function(){
+        let paragraphe = document.createElement(`p`)
+        paragraphe.innerHTML=
+        ecran.appendChild(paragraphe)
+})
