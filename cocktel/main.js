7b4d5fa9f228a1de181f09788d44513897bd263d
Maximilien Roussel <ro.maximilien@gmail.com>
2022-08-16 11:24:32 +0200

    search function

--- cocktel/main.js
+++ cocktel/main.js
@@ -1,6 +1,10 @@
+//stape 1 affiche les info dans la console 
 var menu
+var recherche=document.getElementById("search")
+const ecran = document.getElementById("container");
+
 alert('ok')
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s= ")
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
 .then(response=>response.json())
 .then(data=>{
         menu=data;
@@ -8,3 +12,19 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s= ")
         
     
     })
+//stape 2 affiche écran
+var commande = document.getElementById("search").value;
+ 
+
+  function affiche() {
+     console.log(recherche.value)
+ }
+ recherche.addEventListener("keyup",affiche)
+
+ 
+
+ //test 
+
+
+ 
+//stape 3 adapter selon la recherche  
